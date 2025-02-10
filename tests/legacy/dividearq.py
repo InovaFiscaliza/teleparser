cd46737481228d0ed93fe34969c5cf0812cb5cf9
ronaldokun <ronaldokun@gmail.com>
2025-02-10 16:52:17 -0300

    ♻️ Improve readability of legacy record splitting script
    This commit refactors the legacy `dividearq.py` script to improve code clarity and maintainability:
    
    - Renamed the main function from `div` to `split_records_by_prefix` to better describe its purpose
    - Improved variable naming throughout:
      - `xx` -> `split_length`
      - `car` -> `base_path`
      - `cdb` -> `cdr_database`
      - `da` -> `input_file_path`
      - `pr` -> `prefixes_set`
      - `aae` -> `prefix_mapping`
      - `j` -> `record_line`
      - `b10` -> `current_prefix`
      - `b` -> `mapping_file`
    
    - Removed unused imports
    - Improved code formatting and spacing
    - Maintained exact functionality while making the code more comprehensible
    
    The script continues to handle splitting CDR records into separate files based on their prefixes.

--- tests/legacy/dividearq.py
+++ tests/legacy/dividearq.py
@@ -1,55 +1,46 @@
-﻿def div(xx,car,cdb,da):
-    import os,gzip,binascii,time, datetime,shutil,glob,tarfile,sys,subprocess,io,shelve
-    from datetime import datetime
-    from operator import itemgetter
-    diret=os.getcwd()+'\\'
-    #xx=int(sys.argv[1]);car=sys.argv[2];cdb=sys.argv[3]
-    #da=car+'Acumula'+cdb+'.txt'
-    #xx=20
+﻿def split_records_by_prefix(split_length, base_path, cdr_database, input_file_path):
+    prefixes_set = set()
+    prefix_mapping = {}
+    prefix_file = open(base_path + "Auxiliar" + cdr_database + "\\pr")
+    for prefix in prefix_file:
+        prefix = prefix.replace("/", "-")
 
+        prefixes_set.add(prefix.replace("\n", ""))
+    prefix_file.close()
+    mapping_file = open(base_path + "Auxiliar" + cdr_database + "\\aae")
+    for prefix in mapping_file:
+        prefix = prefix.replace("/", "-")
 
-    pr=set()
-    aae={}
-    a=open(car+'Auxiliar'+cdb+'\\pr')
-    for i in a:
-        i=i.replace('/','-')
+        prefix = prefix.replace("\n", "")
+        prefix_parts = prefix.split("|")
+        prefix_mapping[prefix_parts[0]] = prefix_parts[1]
 
-        pr.add(i.replace('\n',''))
-    a.close()
-    b=open(car+'Auxiliar'+cdb+'\\aae')
-    for i in b:
-        i=i.replace('/','-')
+    mapping_file.close()
 
-        i=i.replace('\n','')
-        ii=i.split('|')
-        aae[ii[0]]=ii[1]
+    for prefix in prefixes_set:
+        prefix = prefix.replace("/", "-")
 
-    b.close()
+        globals()["a" + prefix] = open(
+            base_path + "Arqparciais" + cdr_database + "\\" + prefix + ".txt", "w"
+        )
+    data_file = open(input_file_path)
+    for record_line in data_file:
+        record_line = (
+            record_line[0:split_length].replace("/", "-") + record_line[split_length:]
+        )
 
+        current_prefix = record_line[0:split_length]
+        if current_prefix in prefix_mapping:
+            mapping_file = globals()["a" + prefix_mapping[record_line[0:split_length]]]
+            record_line = (
+                record_line[0:split_length].replace("-", "/")
+                + record_line[split_length:]
+            )
+            mapping_file.write(record_line)
 
-    for i in pr:
-        i=i.replace('/','-')
+    data_file.close()
 
-        globals()['a'+i]=open(car+'Arqparciais'+cdb+'\\'+i+'.txt','w')
-    d=open(da)
-    for j in d:
-        j=j[0:xx].replace('/','-')+j[xx:]
-
-        #print(aae[j[0:xx]])
-        b10=j[0:xx]
-        if b10 in aae:
-            b=globals()['a'+aae[j[0:xx]]]
-            #b=vars()['a'+j[0:xx]]
-            j=j[0:xx].replace('-','/')+j[xx:]
-
-            b.write(j)
-
-
-
-    d.close()
-
-    for i in pr:
-        i=i.replace('/','-')
-        b=globals()['a'+i]
-        b.close()
-    #print(datetime.now()-tr)
+    for prefix in prefixes_set:
+        prefix = prefix.replace("/", "-")
+        mapping_file = globals()["a" + prefix]
+        mapping_file.close()
