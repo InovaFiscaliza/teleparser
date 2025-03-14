40b8c736f7855dc14097edead881efc154696556
ronaldokun <ronaldokun@gmail.com>
2025-03-14 11:05:52 -0300

    ♻️ feat: Add decorator for fixed size unsigned int
    This commit introduces a decorator function, `fixed_size_unsigned_int`, to simplify the creation of `UnsignedInt` classes with a predefined size. This decorator enhances code reusability and readability by encapsulating the size configuration within the class definition.

---
+++ src/teleparser/decoders/ericsson/decorators.py
@@ -0,0 +1,23 @@
+from functools import wraps
+from .primitives import UnsignedInt
+
+
+def fixed_size_unsigned_int(size):
+    """Factory function to create UnsignedInt classes with fixed size"""
+
+    def decorator(cls):
+        original_init = cls.__init__
+
+        @wraps(original_init)
+        def new_init(self, octets, *args, **kwargs):
+            UnsignedInt.__init__(self, octets, size)
+            if (
+                hasattr(original_init, "__code__")
+                and original_init.__code__ != UnsignedInt.__init__.__code__
+            ):
+                original_init(self, octets, *args, **kwargs)
+
+        cls.__init__ = new_init
+        return cls
+
+    return decorator
