37c693298f2d2d0a8bc1c36131ed1fbdab7c7511
ronaldokun <ronaldokun@gmail.com>
2025-03-19 13:06:09 -0300

    ✨ feat: Added fixed_size_ia5_string decorator
    This commit introduces the `fixed_size_ia5_string` decorator. This decorator is designed to be used with `Ia5String` datatypes, ensuring they have a fixed size. This is similar to the existing `fixed_size_digit_string` and `fixed_size_unsigned_int` decorators. The decorator modifies the `__init__` method of the decorated class to enforce the fixed size constraint during initialization. This enhancement provides a consistent way to handle fixed-size IA5 strings within the codebase.

--- src/teleparser/decoders/ericsson/datatypes/decorators.py
+++ src/teleparser/decoders/ericsson/datatypes/decorators.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from .primitives import UnsignedInt, DigitString
+from .primitives import UnsignedInt, DigitString, Ia5String
 
 
 def fixed_size_unsigned_int(size):
@@ -35,6 +35,27 @@ def fixed_size_digit_string(size):
             if (
                 hasattr(original_init, "__code__")
                 and original_init.__code__ != DigitString.__init__.__code__
+            ):
+                original_init(self, octets, *args, **kwargs)
+
+        cls.__init__ = new_init
+        return cls
+
+    return decorator
+
+
+def fixed_size_ia5_string(size):
+    """Factory function to create DigitString classes with fixed size"""
+
+    def decorator(cls):
+        original_init = cls.__init__
+
+        @wraps(original_init)
+        def new_init(self, octets, *args, **kwargs):
+            Ia5String.__init__(self, octets, size)
+            if (
+                hasattr(original_init, "__code__")
+                and original_init.__code__ != Ia5String.__init__.__code__
             ):
                 original_init(self, octets, *args, **kwargs)
 
