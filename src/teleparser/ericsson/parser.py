47770ad11a509ef6807e1b803bb1f9d441e06f6c
ronaldokun <ronaldokun@gmail.com>
2025-03-26 11:22:38 -0300

    ➕ Added: Implemented handling for unknown tags in TlvVozEricsson class

--- src/teleparser/ericsson/parser.py
+++ src/teleparser/ericsson/parser.py
@@ -2,6 +2,8 @@ from collections import UserDict
 from typing import Mapping
 from . import modules
 
+UNKNOWNS_TAGS = {47: modules.RoamingCallForwarding}
+
 
 class TlvVozEricsson:
     """Tag-Length-Value object for BER encoding for Ericsson Common Charging Output"""
@@ -15,8 +17,14 @@ class TlvVozEricsson:
         self.length = len(value)
         if schema is None:  # root
             tag_mapping = modules.CallDataRecord[tag_number]
+        elif self._is_tag_unknown(tag_number, schema):
+            self.name = None
+            self.value = None
+            self.schema = {}
+            return
         else:
             tag_mapping = schema[tag_number]
+
         if isinstance(tag_mapping["type"], dict):
             self.name = tag_mapping["tag"]
             self.value = tag_mapping["name"]
@@ -25,3 +33,6 @@ class TlvVozEricsson:
             self.name = tag_mapping["name"]
             self.value = getattr(tag_mapping["type"](value), "value")
             self.schema = {}
+
+    def _is_tag_unknown(self, tag_number: int, schema: Mapping) -> bool:
+        return tag_number not in schema and UNKNOWNS_TAGS.get(tag_number) == schema
