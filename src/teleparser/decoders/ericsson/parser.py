1b19ccffd4d039b43c3ba2951ef85cebf3a172fd
ronaldokun <ronaldokun@gmail.com>
2025-03-29 12:42:41 -0300

    🗑️ Remove: Delete unused unknown tag logic from Ericsson parser
    This commit removes the logic related to handling unknown tags in the Ericsson parser.
    
    - Removed the `UNKNOWNS_TAGS` dictionary, which was used to define known unknown tags.
    - Removed the `_is_tag_unknown` method from the `TlvVozEricsson` class.
    - Removed the conditional logic that checked for unknown tags during TLV parsing.
    
    These elements were deemed unnecessary, as the `BerDecoder` now handles `KeyError` exceptions, allowing processing to continue when encountering unknown tags. This change simplifies the code and removes redundant logic.

--- src/teleparser/decoders/ericsson/parser.py
+++ src/teleparser/decoders/ericsson/parser.py
@@ -1,22 +1,6 @@
 from typing import Mapping
 from . import modules
 
-UNKNOWNS_TAGS = {
-    47: (modules.RoamingCallForwarding,),
-    130: (modules.RoamingCallForwarding,),
-    28: (modules.INIncomingCall, modules.INOutgoingCall),
-    103: (modules.CallForwarding,),
-    143: (modules.MSOriginating,),
-    154: (modules.MSOriginating,),
-    151: (modules.MSOriginating,),
-    43: (modules.MSTerminatingSMSinMSC,),
-    116: (modules.MSTerminating,),
-    119: (modules.MSTerminating,),
-    17: (modules.MSOriginatingSMSinMSC,),
-    33: (modules.MSOriginatingSMSinMSC,),
-    13: (modules.SSProcedure,),
-}
-
 
 class TlvVozEricsson:
     """Tag-Length-Value object for BER encoding for Ericsson Common Charging Output"""
@@ -30,14 +14,8 @@ class TlvVozEricsson:
         self.length = len(value)
         if schema is None:  # root
             tag_mapping = modules.CallDataRecord[tag_number]
-        elif self._is_tag_unknown(tag_number, schema):
-            self.name = None
-            self.value = None
-            self.schema = {}
-            return
         else:
             tag_mapping = schema[tag_number]
-
         if isinstance(tag_mapping["type"], dict):
             self.name = tag_mapping["tag"]
             self.value = tag_mapping["name"]
@@ -46,8 +24,3 @@ class TlvVozEricsson:
             self.name = tag_mapping["name"]
             self.value = getattr(tag_mapping["type"](value), "value")
             self.schema = {}
-
-    def _is_tag_unknown(self, tag_number: int, schema: Mapping) -> bool:
-        return tag_number not in schema and any(
-            schema == d for d in UNKNOWNS_TAGS.get(tag_number, [])
-        )
